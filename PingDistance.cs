34fcc1ea0e6d1a8e2146c348c179862a43b3ca49
underscorea <53173h@gmail.com>
2019-04-19 23:58:47 +0100

    ping distance mod

---
+++ PingDistance.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using RoR2;
+using RoR2.UI;
+using UnityEngine;
+
+namespace underscorea
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.underscorea.pingdistance", "PingDistance", "1.0")]
+    public class PingDistance
+    {
+        public void Awake()
+        {
+            Debug.Log("PingDistance is awake, rise and shine.");
+        }
+
+        public void Update()
+        {
+            if (!Stage.instance)
+            {
+                return;
+            }
+
+            Vector3 origin = new Vector3();
+            var localPlayers = NetworkUser.readOnlyLocalPlayersList;
+            if (localPlayers.Count > 0)
+            {
+                // I don't know whether I can show different distances for different local players...
+                var target = localPlayers[0].cameraRigController.target;
+                if (target)
+                {
+                    origin = target.GetComponent<CharacterBody>().footPosition;
+                }
+            }
+
+            foreach (PingIndicator pi in PingIndicator.instancesList)
+            {
+                if (pi.enabled) // not sure if this is needed
+                {
+                    float dist = Vector3.Distance(origin, pi.transform.position);
+                    int index = pi.pingText.text.IndexOf((char)0x200B);
+                    string sub = index >= 0 ? pi.pingText.text.Substring(0, index) : pi.pingText.text;
+                    // I don't like this, but I also don't like having to call multiple GetComponent<T> just to get the pingText
+                    pi.pingText.text = sub + (char)0x200B + " (" + dist.ToString("0.0") + "m)";
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Ping Distance
+very simple mod that shows how far the ping is
+i dont know whether it works
+i dont know how well it works
+but it worked for me
+¯\\\_(ツ)\_/¯
