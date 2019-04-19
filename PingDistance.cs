ae4f01a718cbd50f7fb9b99af87e1dbeb02bd09a
underscorea <53173h@gmail.com>
2019-04-20 00:41:05 +0100

    maybe shouldnt spam GetComponent<T> when not used

--- PingDistance.cs
+++ PingDistance.cs
@@ -21,27 +21,30 @@ namespace underscorea
                 return;
             }
 
-            Vector3 origin = new Vector3();
-            var localPlayers = NetworkUser.readOnlyLocalPlayersList;
-            if (localPlayers.Count > 0)
+            if (PingIndicator.instancesList.Count > 0)
             {
-                // I don't know whether I can show different distances for different local players...
-                var target = localPlayers[0].cameraRigController.target;
-                if (target)
+                Vector3 origin = new Vector3();
+                var localPlayers = NetworkUser.readOnlyLocalPlayersList;
+                if (localPlayers.Count > 0)
                 {
-                    origin = target.GetComponent<CharacterBody>().footPosition;
+                    // I don't know whether I can show different distances for different local players...
+                    var target = localPlayers[0].cameraRigController.target;
+                    if (target)
+                    {
+                        origin = target.GetComponent<CharacterBody>().footPosition;
+                    }
                 }
-            }
 
-            foreach (PingIndicator pi in PingIndicator.instancesList)
-            {
-                if (pi.enabled) // not sure if this is needed
+                foreach (PingIndicator pi in PingIndicator.instancesList)
                 {
-                    float dist = Vector3.Distance(origin, pi.transform.position);
-                    int index = pi.pingText.text.IndexOf((char)0x200B);
-                    string sub = index >= 0 ? pi.pingText.text.Substring(0, index) : pi.pingText.text;
-                    // I don't like this, but I also don't like having to call multiple GetComponent<T> just to get the pingText
-                    pi.pingText.text = sub + (char)0x200B + " (" + dist.ToString("0.0") + "m)";
+                    if (pi.enabled) // not sure if this is needed
+                    {
+                        float dist = Vector3.Distance(origin, pi.transform.position);
+                        int index = pi.pingText.text.IndexOf((char)0x200B);
+                        string sub = index >= 0 ? pi.pingText.text.Substring(0, index) : pi.pingText.text;
+                        // I don't like this, but I also don't like having to call multiple GetComponent<T> just to get the pingText
+                        pi.pingText.text = sub + (char)0x200B + " (" + dist.ToString("0.0") + "m)";
+                    }
                 }
             }
         }
